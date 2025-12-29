6eb2429f2c366125a4b85ccc4dcfc99fd2f3cbf4
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-12-29 13:49:00 -0500

    added data file to hold json objects

--- routes/forumRouter.js
+++ routes/forumRouter.js
@@ -1,52 +1,21 @@
 const { Router } = require("express");
+const forumRouter = Router();
 
-const forumRouter = Router()
+// Import JSON using require
+const jsonData = require("../data/data.json");
 
-const boards = [
-    {
-        title: "Announcements"
-    },
-    {
-        title: "Patch Notes"
-    },
-    {
-        title: "Discussion"
-    },
-    {
-        title: "Feedback"
-    },
-    {
-        title: "Bug Reports"
-    },
-
-]
-
-const posts = [
-    {
-        board: "discussion",
-        title: "Hi there!",
-        text: "This is the first post to the forum board!",
-        user: "Brian",
-        added: new Date()
-    },
-    {
-        board: "announcement",
-        title: "Welcome to the FCR community!",
-        text: "Food Chain Rumble is a dream project of mine. A passion project to bring a 3D platform brawler game concept to life.",
-        user: "SweetTeets13",
-        added: new Date()
-    },
-]
+const posts = jsonData.posts;
+const boards = jsonData.boards;
 
 forumRouter.get("/", (req, res) => {
     res.render("forum/forum", {
-        posts: posts,
-        boards: boards
+        posts,
+        boards
     });
 });
 
-forumRouter.get("/newPost", (req,res) => {
-    res.render("forum/newPost")
+forumRouter.get("/newPost", (req, res) => {
+    res.render("forum/newPost");
 });
 
 module.exports = forumRouter;
