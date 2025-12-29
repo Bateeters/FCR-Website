da35faf3aada6a8539ac43c177d92e71c70119b8
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-12-29 17:02:12 -0500

    updated newPost form to populate submitted posts on the forum page

--- routes/forumRouter.js
+++ routes/forumRouter.js
@@ -18,4 +18,23 @@ forumRouter.get("/newPost", (req, res) => {
     res.render("forum/newPost");
 });
 
+// POST new post
+forumRouter.post("/new", (req, res) => {
+    const { title, text, user, board } = req.body;
+
+    const newPost = {
+        id: Date.now().toString(),
+        board,
+        title,
+        text,
+        user,
+        added: new Date().toLocaleDateString('en-US')
+    };
+
+    posts.push(newPost);
+
+    // Later: write back to JSON or DB
+    res.redirect("/forum");
+});
+
 module.exports = forumRouter;
